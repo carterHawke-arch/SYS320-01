9c22476337859d37f8b90af190cb010b40f83042
carter <carter.hawke@mymail.champlain.edu>
2026-02-11 14:56:44 -0500

    week 5

---
+++ week 5/WebRequests.ps1
@@ -0,0 +1,6 @@
+﻿$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.9/ToBeScraped.html
+
+$divs1=$scraped_page.ParsedHtml.body.getElementsByTagName("div") | where {
+$_.getAttributeNode("class").value -ilike "div-1"} | Select innerText
+
+$divs1
---
+++ week 5/mainScrape.ps1
@@ -0,0 +1,26 @@
+﻿. "C:\Users\Carter\SYS320-01\week 5\ScrapeChamplain.ps1"
+
+#daysTranslator(gatherClasses) | 
+#    Select-Object "Class Code", Instructor, Location, Days, "Time Start", "Time End" | 
+#    Where-Object { $_.Instructor -ilike "*Furkan*" }
+
+#daysTranslator(gatherClasses) | Where-Object { ($_."Location" -ilike "FREE 105") -and ($_.days -contains "Wednesday") } |
+#            Sort-Object "Time Start" |
+#            Select-Object "Time Start", "Time End", "Class Code"
+
+
+#$ITSInstructors = daysTranslator(gatherClasses) | Where-Object { 
+#    ($_."Class Code" -ilike "SYS*") -or
+#    ($_."Class Code" -ilike "NET*") -or
+#    ($_."Class Code" -ilike "SEC*") -or
+#    ($_."Class Code" -ilike "FOR*") -or
+#    ($_."Class Code" -ilike "CSI*") -or
+#    ($_."Class Code" -ilike "DAT*") 
+#    } `
+#    | Select-Object "Instructor" -Unique `
+#    | Sort-Object Instructor
+
+#$ITSInstructors
+
+$FullTable = daysTranslator(gatherClasses)
+$FullTable | Where {$_.Instructor -in $ITSInstructors.Instructor } | Group "Instructor" | Select count, name | Sort-Object Count -Descending
